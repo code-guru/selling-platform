3b5c7ff4984c285bc8ec547fa5ba8354f3058200
Mehdi Tirgar <mtirgar@gmail.com>
2020-06-09 19:39:02 +0200

    Added database configs

---
+++ src/Application.kt
@@ -0,0 +1,60 @@
+package lvls
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+import org.slf4j.event.*
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(AutoHeadResponse)
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(ConditionalHeaders)
+
+    initDB()
+
+    routing {
+
+    }
+}
+
+fun initDB() {
+    val config = HikariConfig("/hikari.properties")
+    config.schema = "public"
+    val ds = HikariDataSource(config)
+    Database.connect(ds)
+}
